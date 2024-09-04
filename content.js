0be487acf6df8ce8fa4ad473f39167ace48068ff
Renan Kataoka <renankataoka@MacBook-Pro-de-Renan.local>
2024-09-04 13:36:46 -0300

    Cleanup + excluded group messages
    Signed-off-by: Renan Kataoka <renankataoka@MacBook-Pro-de-Renan.local>

--- content.js
+++ content.js
@@ -3,21 +3,11 @@ function connect() {
 	webSocket = new WebSocket('wss://equipped-concise-owl.ngrok-free.app/cable');
 
 	webSocket.onopen = (event) => {
-		console.log('websocket open');
-
-		// const subscribeCommand = {
-		// 	command: 'subscribe',
-		// 	identifier: JSON.stringify({
-		// 		id: 'xpto',
-		// 		channel: 'RoomChannel'
-		// 	}),
-		// };
-
 		const subscribeCommand = {
 			command: 'subscribe',
 			identifier: JSON.stringify({
 				id: 'fbdd54b9-d7cf-441b-bdbd-1573f258c221',
-				channel: 'WhatsppWebClientUserChannel'
+				channel: 'WhatsappWebClientUserChannel'
 			}),
 		};
 
@@ -70,6 +60,12 @@ function disconnect() {
 	webSocket.close();
 }
 
+function reconnectIfNeeded() {
+	if (webSocket == null) {
+		connect();
+	}
+}
+
 function keepAlive() {
 	const keepAliveIntervalId = setInterval(
 		() => {
@@ -83,115 +79,43 @@ function keepAlive() {
 	);
 }
 
+function sendActionToWebsocket(action, body) {
+	const payload = {
+		command: 'message',
+		data: JSON.stringify({
+			action: action,
+			body: JSON.stringify(body)
+		}),
+		identifier: JSON.stringify({
+			id: 'fbdd54b9-d7cf-441b-bdbd-1573f258c221',
+			channel: 'WhatsappWebClientUserChannel'
+		}),
+	};
+
+	webSocket.send(JSON.stringify(payload));
+}
+
 connect()
 
 window.addEventListener("ConversationReceived", event => {
-	if (webSocket == null) {
-		connect();
-	}
-
-	console.log("ConversationReceived")
-
-	// const fullConversation = {
-	// 	command: 'message',
-	// 	data: JSON.stringify({
-	// 		action: 'display_conversation',
-	// 		body: JSON.stringify(event.detail)
-	// 	}),
-	// 	identifier: JSON.stringify({
-	// 		id: 'xpto',
-	// 		channel: 'RoomChannel'
-	// 	}),
-	// };
-
-	const conversationPayload = {
-		command: 'message',
-		data: JSON.stringify({
-			action: 'display_conversation_on_chat',
-			body: JSON.stringify(event.detail)
-		}),
-		identifier: JSON.stringify({
-			id: 'fbdd54b9-d7cf-441b-bdbd-1573f258c221',
-			channel: 'WhatsppWebClientUserChannel'
-		}),
-	};
-
-	webSocket.send(JSON.stringify(conversationPayload));
+	reconnectIfNeeded()
+	sendActionToWebsocket('display_conversation_on_chat', event.detail);
 });
 
 window.addEventListener("MessageSent", event => {
-	if (webSocket == null) {
-		connect();
-	}
-
-	// const messageDeliveredNotification = {
-	// 	command: 'message',
-	// 	data: JSON.stringify({
-	// 		action: 'notify_message_sent',
-	// 		body: JSON.stringify(event.detail)
-	// 	}),
-	// 	identifier: JSON.stringify({
-	// 		id: 'xpto',
-	// 		channel: 'RoomChannel'
-	// 	}),
-	// };
-
-	const messageDeliveredNotificationPayload = {
-		command: 'message',
-		data: JSON.stringify({
-			action: 'display_message_delivered_on_chat',
-			body: JSON.stringify(event.detail)
-		}),
-		identifier: JSON.stringify({
-			id: 'fbdd54b9-d7cf-441b-bdbd-1573f258c221',
-			channel: 'WhatsppWebClientUserChannel'
-		}),
-	};
-
-	webSocket.send(JSON.stringify(messageDeliveredNotificationPayload));
+	reconnectIfNeeded()
+	sendActionToWebsocket('display_message_delivered_on_chat', event.detail);
 });
 
 window.addEventListener("NewMessageArrived", event => {
+	reconnectIfNeeded()
+
 	const message = {
 		...event.detail,
 		direction: 'outbound',
 		status: 'delivered',
-	}
-
-	// const messagePayload = {
-	// 	command: 'message',
-	// 	data: JSON.stringify({
-	// 		action: 'display_received_message',
-	// 		body: JSON.stringify(message)
-	// 	}),
-	// 	identifier: JSON.stringify({
-	// 		id: 'xpto',
-	// 		channel: 'RoomChannel'
-	// 	}),
-	// };
-
-	const messagePayload = {
-		command: 'message',
-		data: JSON.stringify({
-			action: 'display_message_received_on_chat',
-			body: JSON.stringify(message)
-		}),
-		identifier: JSON.stringify({
-			id: 'fbdd54b9-d7cf-441b-bdbd-1573f258c221',
-			channel: 'WhatsppWebClientUserChannel'
-		}),
 	};
-
-	webSocket.send(JSON.stringify(messagePayload));
-	// fetch("https://jsonplaceholder.typicode.com/todos", {
-	// 	method: "POST",
-	// 	body: JSON.stringify(event.msg),
-	// 	headers: {
-	// 		"Content-type": "application/json; charset=UTF-8"
-	// 	}
-	// }).then((response) => response.json())
-	// 	.then((json) => console.log(json));
-	// console.log("from extension: " + event.detail);
+	sendActionToWebsocket('display_message_delivered_on_chat', message);
 });
 
 //esse content.js é executado isolado da página e por isso tem acesso à api do chrome extension, como chrome.rintme
--- my-code.js
+++ my-code.js
@@ -1,47 +1,84 @@
 (async () => {
-	//todo código dentro de um escopo pra não conflitar com possíveis variáveis definidas na página do zap
-
-	//espera WPP estar pronta pra usar
 	await new Promise(r => {
 		const interval = setInterval(() => {
 			if (WPP.isFullReady) {
 				clearInterval(interval);
 				r();
 			}
-		});
+		}
+		);
 	});
 
-	//usa WPP
-	const myProfileName = WPP.profile.getMyProfileName();
-	console.log("from script: " + myProfileName);
+	function isGroupMessage(msg) {
+		console.log(`Is Group Message: ${msg.__x_author.user !== msg.__x_from.user}`)
+		return msg.__x_author.user !== msg.__x_from.user;
+	}
 
-	WPP.on('chat.new_message', (msg) => {
+	WPP.on('chat.new_message', async (msg) => {
+		if (isGroupMessage(msg)) return;
+
+		const senderNumber = msg.__x_author.user
+		const profilePicUrl = await WPP.contact.getProfilePictureUrl(`${senderNumber}@c.us`);
+
 		window.dispatchEvent(new CustomEvent("NewMessageArrived", {
-			detail: buildMsgPayload(msg)
+			detail: buildMsgPayload(msg, '', profilePicUrl)
 		}));
 	});
 
 	WPP.on('chat.active_chat', (chat) => {
-		console.log(chat)
-		// Abrir janela Bolten com contato Bolten
+		// console.log(chat)
 	});
 
 
-	buildMsgPayload = (msg, senderNumber = '') => {
+	buildMsgPayload = (msg, senderNumber = '', profilePicUrl = undefined) => {
+		console.log(msg)
+		const profilePicThumb = msg.__x_senderObj?.__x_profilePicThumb?.__x_imgFull || profilePicUrl;
+
 		return {
 			id: msg.__x_id?.id,
 			rowId: msg.__x_rowId,
 			body: msg.__x_body,
-			from: msg.__x_from?.user,
+			from: msg.__x_author?.user,
 			to: msg.__x_to?.user,
 			type: msg.__x_type,
-			direction: msg.__x_from == senderNumber ? 'outbound' : 'inbound',
+			direction: msg.__x_author?.user == senderNumber ? 'outbound' : 'inbound',
 			senderName: msg.__x_senderObj?.pushname,
 			senderShortName: msg.__x_senderObj?.shortName,
-			profilePicThumb: msg.__x_senderObj?.__x_profilePicThumb?.__x_imgFull,
+			profilePicThumb: profilePicThumb,
+			quotedMsg: msg.__x_quotedMsg?.body,
 			timestamp: new Date(msg.__x_t * 1000),
 		}
 	}
+
+	window.addEventListener("ConversationRequested", async event => {
+		const formattedPersonNumber = event.detail.recipientNumber.slice(1, event.detail.recipientNumber.length);
+		const messageLimit = event.detail.messageLimit;
+		const messages = await WPP.chat.getMessages(formattedPersonNumber, { count: messageLimit });
+		const profilePicUrl = await WPP.contact.getProfilePictureUrl(`${formattedPersonNumber}@c.us`);
+		const formattedMessages = messages.map(msg => {
+			return buildMsgPayload(msg, formattedPersonNumber, profilePicUrl);
+		})
+
+		window.dispatchEvent(new CustomEvent("ConversationReceived", {
+			detail: formattedMessages
+		}));
+	})
+	window.addEventListener("MessageDispatchRequested", async event => {
+		const formattedRecipientNumber = event.detail.recipientNumber.slice(1, event.detail.recipientNumber.length);
+		const message = await WPP.chat.sendTextMessage(formattedRecipientNumber, event.detail.message, { createChat: true });
+
+		window.dispatchEvent(new CustomEvent("MessageSent", {
+			detail: {
+				dispatchId: message.id,
+				id: event.detail.externalId,
+				direction: 'inbound',
+				from: message.from.split("@")[0],
+				to: message.to.split("@")[0],
+				senderName: 'You',
+				timestamp: new Date(),
+			}
+		}));
+	})
 
 	// // List only chats with users
 	// chats = await WPP.chat.list({ onlyUsers: true });
@@ -61,35 +98,4 @@
 	// 	contactData[nd[1]] = msgs
 	// 	console.log(msgs)
 	// });
-
-	window.addEventListener("ConversationRequested", async event => {
-		// console.log(`ConversationRequested: ${event.detail}`);
-		const formattedSenderNumber = event.detail.senderNumber.slice(1, event.detail.senderNumber.length);
-		const messageLimit = event.detail.messageLimit;
-		const messages = await WPP.chat.getMessages(formattedSenderNumber, { count: messageLimit });
-		const formattedMessages = messages.map(msg => {
-			return buildMsgPayload(msg, formattedSenderNumber);
-		})
-
-		window.dispatchEvent(new CustomEvent("ConversationReceived", {
-			detail: formattedMessages
-		}));
-	})
-	window.addEventListener("MessageDispatchRequested", async event => {
-		// console.log(`ConversationRequested: ${event.detail}`);
-		const formattedRecipientNumber = event.detail.recipientNumber.slice(1, event.detail.recipientNumber.length);
-		const message = await WPP.chat.sendTextMessage(formattedRecipientNumber, event.detail.message, { createChat: true });
-
-		window.dispatchEvent(new CustomEvent("MessageSent", {
-			detail: {
-				// id: message.id,
-				id: event.detail.externalId,
-				direction: 'inbound',
-				from: message.from.split("@")[0],
-				to: message.to.split("@")[0],
-				senderName: 'You',
-				timestamp: new Date(),
-			}
-		}));
-	})
 })();
